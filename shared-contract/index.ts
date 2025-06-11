78030c81e943dc64d6a45a68994cd742935741d8
kusainovv <kusainovratmir89@gmail.com>
2025-06-11 13:11:25 +0400

    Update shared contract to version 0.1.7: Add DTOs for user authentication and management (LoginDto, RegisterDto, CreateUserDto, UpdateUserDto) and enable experimental decorators in tsconfig.json.

--- shared-contract/index.ts
+++ shared-contract/index.ts
@@ -1,3 +1,8 @@
 export * from './schemas/user';
 export * from './schemas/employee';
 export * from './schemas/organization';
+
+export * from './dto/auth/login.dto';
+export * from './dto/auth/register.dto';
+export * from './dto/user/create-user.dto';
+export * from './dto/user/update-user.dto';
